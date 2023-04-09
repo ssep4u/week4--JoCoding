7d1d072ab36f17d652773b44afc558ff1d156380
SSep <ssep4u@gmail.com>
2023-04-09 16:59:13 +0900

    /dog, /cat

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const port = 3000
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+app.get('/dog', (req, res) => {
+    res.send('<h1>강아지</h1>')
+})
+app.get('/cat', (req, res) => {
+    res.send('<h2>고양이</h2>')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
