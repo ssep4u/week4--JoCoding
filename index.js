ff74a8ec9bc89c19b6e2ecb5556aa52762386734
SSep <ssep4u@gmail.com>
2023-04-09 17:03:06 +0900

    /dog -> res.json()

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 app.get('/dog', (req, res) => {
-    res.send('<h1>강아지</h1>')
+    res.json({'sound': '멍멍'})
 })
 app.get('/cat', (req, res) => {
-    res.send('<h2>고양이</h2>')
+    res.json({'sound': '야옹'})
 })
 
 app.listen(port, () => {
